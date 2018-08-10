577cf7d1fc2c82a7a6fa00e3f12597a0ec06a5e4
Adam Duff <j.adam.duff@gmail.com>
2018-08-10 02:35:45 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,7 +6,7 @@ end
 WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [2, 5, 8], [1, 4, 7], [0, 4, 8], [2, 4, 6]]
 
 def won?(board)
-  if board.any? {|pos| position_taken?(board, pos)} == true
+  if board.any? {|pos| board[pos] == "X" || board[pos] == "O" }
     return false
   end
 end
